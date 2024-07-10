ea1e560dc8823c9ae0a306015952f39c02df9a30
joinkudehelp <165511169+joinkudehelp@users.noreply.github.com>
2024-07-10 15:54:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(cors());
 const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
-const userEmail = "makinmoni247@gmail.com";
-const pass = "zusxabzqbuqlxyis";
+const userEmail = "joinkudehelp@gmail.com";
+const pass = "hzansupprovrhavt";
 
 // Middleware
 app.use(express.json());
