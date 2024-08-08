6919476b4e45ec6e11f48c705ec283dd13795294
joinkudehelp <165511169+joinkudehelp@users.noreply.github.com>
2024-08-08 11:40:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "joinkudehelp@gmail.com";
-const pass = "hzansupprovrhavt";
+//const pass = "hzansupprovrhavt";
 
 // Middleware
 app.use(express.json());
